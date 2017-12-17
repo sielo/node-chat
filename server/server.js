aa5231ec288dd13c77623c35b3c99d512370770c
Michal Jasinski <michal@iMac-2.local>
2017-12-17 17:29:25 +0100

    broadcast

--- server/server.js
+++ server/server.js
@@ -18,6 +18,17 @@ app.use(express.static(publicPath));  // wskazujemy z którego folderu pobierać
 io.on('connection', (socket) => {
     console.log('New user connected:', socket);
 
+    socket.emit('newMessage', {    // emituje zdarzenia do TEGO podłączonego
+        from: 'admin',
+        text: `Welcome to chat...`,
+        createdAt: new Date().getTime()
+    });  
+    socket.broadcast.emit('newMessage', {    // emituje zdarzenia do WSZYSTKICH podłączonych POZA wywołującym
+        from: 'admin',
+        text: `User joined the chat...`,
+        createdAt: new Date().getTime()
+    });  
+
     // socket.emit('newMessage', {   // emituje zdarzenie tylko do tego socketa
     //     from: 'Michał',
     //     text: 'Cześć Zdzichu!',
@@ -26,11 +37,21 @@ io.on('connection', (socket) => {
 
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
-        io.emit('newMessage', {
+
+       
+        // Poniszy kod emituje zdarzenie do wszystkich podłączonych
+        io.emit('newMessage', {    // emituje zdarzenia do WSZYSTKICH podłączonych
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
-        });  // emituje zdarzenia do WSZYSTKICH podłączonych
+        });  
+
+        // Poniszy kod emituje zdarzebie do wszystkich POZA tym który jest w "socket" (czyli tym który dołączył do chatu)
+        // socket.broadcast.emit('newMessage', {    // emituje zdarzenia do WSZYSTKICH podłączonych
+        //         from: message.from,
+        //         text: message.text,
+        //         createdAt: new Date().getTime()
+        //     });
     });
     socket.on('disconnect', () => {
         console.log('User disconnected!', socket)
